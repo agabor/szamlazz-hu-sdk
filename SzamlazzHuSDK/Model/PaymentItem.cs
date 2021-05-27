574ae4c4c974087a76863b84361c408e45de0268
Gabor Angyal <angyalgabor@outlook.com>
2021-05-27 17:52:15 +0200

    update package

--- SzamlazzHuSDK/Model/PaymentItem.cs
+++ SzamlazzHuSDK/Model/PaymentItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SzamlazzHuSDK.Model
+namespace SzamlazzHu
 {
     public class PaymentItem
     {
--- SzamlazzHuSDK/Xml/XmlParser.cs
+++ SzamlazzHuSDK/Xml/XmlParser.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Xml;
-using SzamlazzHuSDK.Model;
 
 namespace SzamlazzHu
 {
