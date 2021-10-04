63a2c8a286bc57c7f5b9b9fe35a53518293a106e
safigi <istvan.safar@gmail.com>
2021-10-04 17:05:22 +0200

    Update InvoiceItem.cs
    float ws decimal

--- SzamlazzHuSDK/Model/InvoiceItem.cs
+++ SzamlazzHuSDK/Model/InvoiceItem.cs
@@ -5,7 +5,7 @@ namespace SzamlazzHu
         public string Name { get; set; }
         public decimal Quantity { get; set; }
         public string UnitOfQuantity { get; set; }
-        public float UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
         public string VatRate { get; set; }
         public decimal NetPrice { get; set; }
         public decimal VatAmount { get; set; }
