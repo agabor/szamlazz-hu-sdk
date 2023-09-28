f4a59202bdc5815d32b1d18a99278cd620869475
Gabor Angyal <gabor.angyal@codesharp.hu>
2023-09-28 14:23:17 +0200

    add query tax payer test

---
+++ SzamlazzHuTest/TaxPayerTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SzamlazzHu;
+
+namespace SzamlazzHuTest;
+
+[TestClass]
+public class TaxPayerTest
+{
+    [TestMethod]
+    public async void QueryTaxpayer()
+    {
+        var api = new SzamlazzHuApi();
+        string apiKey = Environment.GetEnvironmentVariable("SZAMLAZZ_HU_KEY");
+        if (string.IsNullOrEmpty(apiKey))
+            Assert.Inconclusive("Environment variable SZAMLAZZ_HU_KEY is not set.");
+        var taxPayerResponse = await api.QueryTaxpayer(new QueryTaxpayerRequest { AuthenticationData = new AuthenticationData { ApiKey = apiKey }, TaxpayerId = "27273209" });
+        Assert.AreEqual("Code Sharp Kft.", taxPayerResponse.Taxpayer.TaxpayerShortName);
+    }
+}
